cc2d6441e81dc55ea071fb267be2b208a4c7cf4d
Felipe Cesar <fecef.figueiredo@gmail.com>
2025-11-21 01:18:53 -0300

    ♻️ refactor(api): change base URL to production
    - change base URL to production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://senac-pti-back.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
