3c0229011dd920e3ef364fff38a2b3f6dda6677c
Felipe Cesar <fecef.figueiredo@gmail.com>
2025-11-24 20:46:13 -0300

    ✨ feat(routes): add routes for customer and product registration
    - implement new routes for customer registration, product registration, and customer details
    - enable navigation to customer and product management pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,6 @@ import RegisterCustomer from "../pages/RegisterCustomer";
 import RegisterProduct from "../pages/RegisterProduct";
 import Dashboard from "../pages/dashboard";
 
-
-
 export function RoutesMain() {
   return (
     <Routes>
@@ -13,7 +11,6 @@ export function RoutesMain() {
       <Route index path="/customer/:cpf" element={<CustomerDetail />} />
       <Route path="/customer" element={<RegisterCustomer />} />
       <Route path="/product" element={<RegisterProduct />} />
-
     </Routes>
   );
 }
